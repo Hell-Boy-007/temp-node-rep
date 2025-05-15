8310d15450fe6072b06332183f21dbb4cb12b3b4
Hell-Boy-007 <tanishqmajumderr@gmail.com>
2025-05-15 11:58:50 +0530

    initial project setup with package.json and package-lock.json

--- app.js
+++ app.js
@@ -8,6 +8,11 @@
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (mac)
 
+// package.json - manifest files (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
 const _ = require('lodash');
 const items = [1,[2],[3,[4]]]
 const newItems = _.flattenDeep(items);
